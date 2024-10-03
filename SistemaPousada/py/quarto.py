1fcdea324b53d37ed9594fbb135c32bb6f09ba94
Dafni <rosadafni0@gmail.com>
2024-10-03 11:30:01 -0300

    Fix: quarto.py + produto.py

--- SistemaPousada/py/quarto.py
+++ SistemaPousada/py/quarto.py
@@ -1,4 +1,5 @@
 from produto import Produto
+
 class Quarto:
      # Verificar sobre o valor do atributo 'categoria' (char)
      # Criando o método init para inicializar a classe
@@ -10,16 +11,21 @@ class Quarto:
         self.consumo = []           # Lista de códigos de produtos consumidos
         self.codigo = codigo
     
-    def adiciona_consumo(self, codigo):
+    def adiciona_consumo(self, produto):
         """Adiciona um produto à lista de consumo."""
-        self.consumo.append(codigo)
+        self.consumo.append(produto)
         print(self.consumo)
     
     def lista_consumo(self):
         for item in self.consumo:
             print(item)
     
-    def valor_total_consumo(self, preco):
+    def valor_total_consumo(self):
+        total = 0
+        for produto in self.consumo:
+            total += produto.preco
+        return total
+        
 
     def limpa_consumo(self):
         """Limpa o consumo após o check-out."""
@@ -31,7 +37,25 @@ class Quarto:
 
 def main():
      
-     j = Produto(1, "Café", 5.00)
-     i = Quarto
-     i.adiciona_consumo(j)
+    produto1 = Produto(1, "Café", 5.00)
+    produto2 = Produto(2, "Chá", 4.00)
+    produto3 = Produto(3,"Água", 2.00)
+
+    quarto1 = Quarto(1,1.000 ,"S", "L")
+
+    quarto1.adiciona_consumo(produto1)
+    quarto1.adiciona_consumo(produto2)
+    quarto1.adiciona_consumo(produto3)
+
+    print("\nProdutos consumidos:")
+    quarto1.lista_consumo()
+
+
+    total = quarto1.valor_total_consumo()
+    print(f"Valor total consumo:{total}")
+
+    """j = Produto(1, "Café", 5.00)
+       i = Quarto
+       i.adiciona_consumo(j) """
+
 main()
