52eceebec18f7e4707bf7f5c91be3419b4ca612b
ghizziz <eduardodecamargo11@gmailcom>
2024-10-05 16:03:03 -0300

    Co-authored-by: Dafni Rosa <dafnirca@users.noreply.github.com>

--- SistemaPousada/py/quarto.py
+++ SistemaPousada/py/quarto.py
@@ -19,19 +19,17 @@ class Quarto:
         for item in self.consumo:
             print(item)
     
-    def valor_total_consumo(self, preco):
+    # def valor_total_consumo(self, preco):
 
     def limpa_consumo(self):
         """Limpa o consumo após o check-out."""
         self.consumo.clear()
+        return
 
     # Criando o método para imprimir as informações da classe Quarto
     def __str__(self):
         return f"Quarto {self.numero} - Categoria: {self.categoria}, Status: {self.status}, Diária: R$ {self.diaria:.2f}"
 
 def main():
-     
-     j = Produto(1, "Café", 5.00)
-     i = Quarto
-     i.adiciona_consumo(j)
+    
 main()
